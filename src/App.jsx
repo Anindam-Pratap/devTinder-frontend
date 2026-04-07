121cf3d1dd196bc439a419e7d0f18396f035d633
Anindam-Pratap <anindam835@gmail.com>
2026-04-07 18:32:24 +0530

    setup react-redux-store, and updated the login feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { BrowserRouter ,Route, Routes} from "react-router-dom"
 import Body from "./components/Body"
 import Login from "./components/Login"
+import { Provider } from "react-redux"
+import appStore from "../utils/appStore"
+import Feed from "./components/Feed"
 
 
 
 function App() {
   
   return (
+    <div>
+    <Provider store={appStore}>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Body />}>
         <Route path="/login" element={<Login />} />
+         <Route path="/" element={<Feed />} />
       </Route>
     </Routes>
 </BrowserRouter>
-  
+</Provider>
+  </div>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import axios from "axios"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { addUser } from "../../utils/userSlice"
+import { useNavigate } from "react-router-dom"
+
 
 const Login = () => {
+const [emailId,SetEmailId] = useState("lukadoncic@gmail.com")
+const [password,SetPassword] = useState("Lukadoncic@123")
+const dispatch = useDispatch()
+const navigate = useNavigate()
+
+const loginUser = async() =>{
+    const res = await axios.post(
+        "http://localhost:3000/login", 
+    {emailId,password},
+{withCredentials:true})
+
+dispatch(addUser(res.data))
+navigate("/")
+}
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-20'>
+<fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 ">
+  <legend className="fieldset-legend mx-auto">Login</legend>
+
+  <label className="label">Email</label>
+  <input type="email" className="input" placeholder="Email" value={emailId} onChange={(e)=>{SetEmailId(e.target.value)}} />
+
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" value={password} onChange={(e)=>{SetPassword(e.target.value)}} />
+
+  <button className="btn btn-neutral mt-4" onClick={loginUser}>Login</button>
+</fieldset>
+</div>
   )
 }
 
